2b754d3c6259ea32cf0bee666119a8f2625988a0
John-Philip Johansson <seriema@gmail.com>
2013-07-16 17:40:05 +0200

    Basic GitHub connection tests

--- test/npmalerts_test.js
+++ test/npmalerts_test.js
@@ -1,187 +1,247 @@
 'use strict';
 
-var npmalerts = require('../lib/npmalerts.js');
+//var npmalerts = require('../lib/npmalerts.js');
+var GitHubApi = require('github');
+
+var github = new GitHubApi({
+	// required
+	version: "3.0.0",
+	// optional
+	timeout: 5000
+});
+
+var mockPackageJson = {
+  "devDependencies": {
+    "grunt": "~0.4.1",
+    "grunt-contrib-jshint": "~0.2.0",
+    "grunt-contrib-nodeunit": "~0.1.2",
+    "grunt-contrib-watch": "~0.4.4"
+  },
+  "dependencies": {
+    "github": "~0.1.10"
+  }
+}
+
 
 /*
   ======== A Handy Little Nodeunit Reference ========
   https://github.com/caolan/nodeunit
 
   Test methods:
-    test.expect(numAssertions)
-    test.done()
+	test.expect(numAssertions)
+	test.done()
   Test assertions:
-    test.ok(value, [message])
-    test.equal(actual, expected, [message])
-    test.notEqual(actual, expected, [message])
-    test.deepEqual(actual, expected, [message])
-    test.notDeepEqual(actual, expected, [message])
-    test.strictEqual(actual, expected, [message])
-    test.notStrictEqual(actual, expected, [message])
-    test.throws(block, [error], [message])
-    test.doesNotThrow(block, [error], [message])
-    test.ifError(value)
+	test.ok(value, [message])
+	test.equal(actual, expected, [message])
+	test.notEqual(actual, expected, [message])
+	test.deepEqual(actual, expected, [message])
+	test.notDeepEqual(actual, expected, [message])
+	test.strictEqual(actual, expected, [message])
+	test.notStrictEqual(actual, expected, [message])
+	test.throws(block, [error], [message])
+	test.doesNotThrow(block, [error], [message])
+	test.ifError(value)
 */
 
 exports['npmalerts'] = {
-  setUp: function(done) {
-    // setup here
-    done();
-  },
-  'no args': function(test) {
-    test.expect(1);
-    // tests here
-    test.equal(npmalerts.awesome(), 'awesome', 'should be awesome.');
-    test.done();
-  },
-  'connect to GitHub repo': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'read package.json in Github repo': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'find dependencies in package.json': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'find devDependencies in package.json': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'parse package name and version from dependencies': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'get package version from npm using package name': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'parse package version from npm': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'detect new version available': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'detect packages with same version': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'detect new version is within semver limits of package.json': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'detect new version is outside of semver limits of package.json': function(test) {
-      test.expect(0);
-      test.done();
-  },
+	setUp: function(done) {
+		// setup here
+		done();
+	},
+	'connect to GitHub repo': function(test) {
+		test.expect(2);
+		github.repos.get({
+			user: 'seriema',
+			repo: 'prim.js'
+		}, function(error, result) {
+			test.ok(!error);
+			test.ok(result);
+			test.done();
+		});
+	},
+	'get package.json in Github repo': function(test) {
+		test.expect(4);
+		github.repos.getContent({
+			user: 'seriema',
+			repo: 'prim.js',
+			path: 'package.json'
+		}, function(error, result) {
+			test.ok(!error);
+			test.ok(result);
+			test.ok(result.content);
+			test.ok(result.encoding);
+			test.done();
+		});
+	},
+	'read package.json in Github repo': function(test) {
+		test.expect(1);
+		github.repos.getContent({
+			user: 'seriema',
+			repo: 'prim.js',
+			path: 'package.json'
+		}, function(error, result) {
+			var json = new Buffer(result.content, result.encoding).toString();
+			test.ok(json);
+			test.done();
+		});
+	},
+	'find dependencies in package.json': function(test) {
+		test.expect(1);
+		github.repos.getContent({
+			user: 'seriema',
+			repo: 'prim.js',
+			path: 'package.json'
+		}, function(error, result) {
+			var json = new Buffer(result.content, result.encoding).toString();
+			test.ok(JSON.parse(json).dependencies);
+			test.done();
+		});
+	},
+	'find devDependencies in package.json': function(test) {
+		test.expect(1);
+		github.repos.getContent({
+			user: 'seriema',
+			repo: 'prim.js',
+			path: 'package.json'
+		}, function(error, result) {
+			var json = new Buffer(result.content, result.encoding).toString();
+			test.ok(JSON.parse(json).devDependencies);
+			test.done();
+		});
+	},
+	'parse package name and version from dependencies': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'get package version from npm using package name': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'parse package version from npm': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'detect new version available': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'detect packages with same version': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'detect new version is within semver limits of package.json': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'detect new version is outside of semver limits of package.json': function(test) {
+		test.expect(0);
+		test.done();
+	},
 
-  // check package version with storage
-  'get package version from storage using package name': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'do not check npm if stored info is less than 24h': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'check npm when data is old': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'compare latest package version to stored package version': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'detect new package release': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'detect new major version': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'detect new minor version': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'detect patch': function(test) {
-      test.expect(0);
-      test.done();
-  },
+	// check package version with storage
+	'get package version from storage using package name': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'do not check npm if stored info is less than 24h': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'check npm when data is old': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'compare latest package version to stored package version': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'detect new package release': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'detect new major version': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'detect new minor version': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'detect patch': function(test) {
+		test.expect(0);
+		test.done();
+	},
 
-  // notification system
-  'send out email': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'detect email bounce': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'remove email from registry': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'add email and attached Github repository to registry': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'add Github repository to registry': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'remove Github repository from registry': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'detect dangling Github with no attached email': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'get list of Github repositories in registry': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'get list of emails attached to a Github repository': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'store latest notified version of package': function(test) {
-      test.expect(0);
-      test.done();
-  },
+	// notification system
+	'send out email': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'detect email bounce': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'remove email from registry': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'add email and attached Github repository to registry': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'add Github repository to registry': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'remove Github repository from registry': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'detect dangling Github with no attached email': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'get list of Github repositories in registry': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'get list of emails attached to a Github repository': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'store latest notified version of package': function(test) {
+		test.expect(0);
+		test.done();
+	},
 
-  // end-to-end, integration tests
-  'user registers': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'user unregisters': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'cron checks a Github repo': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'cron emails about new versions': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'cron removes dangling Github repos': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'cron goes back to sleep': function(test) {
-      test.expect(0);
-      test.done();
-  },
-  'cron wakes up': function(test) {
-      test.expect(0);
-      test.done();
-  }
+	// end-to-end, integration tests
+	'user registers': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'user unregisters': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'cron checks a Github repo': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'cron emails about new versions': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'cron removes dangling Github repos': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'cron goes back to sleep': function(test) {
+		test.expect(0);
+		test.done();
+	},
+	'cron wakes up': function(test) {
+		test.expect(0);
+		test.done();
+	}
 
 };
