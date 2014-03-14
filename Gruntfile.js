125f5e752be1290aa46b6f75ee1a44a3751a6f08
Tim Caswell <tim@creationix.com>
2014-03-14 19:27:42 +0000

    Style cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,49 +2,49 @@
 
 module.exports = function(grunt) {
 
-    // Project configuration.
-    grunt.initConfig({
-        jshint: {
-            options: {
-                jshintrc: '.jshintrc'
-            },
-            gruntfile: {
-                src: 'Gruntfile.js'
-            },
-            lib: {
-                src: ['lib/*.js', 'lib/controllers/*.js', 'lib/routes/*.js']
-            },
-            test: {
-                src: ['test/**/*.js']
-            }
-        },
-        nodeunit: {
-            files: ['test/**/*.js']
-        },
-        watch: {
-            gruntfile: {
-                files: '<%= jshint.gruntfile.src %>',
-                tasks: ['jshint:gruntfile']
-            },
-            lib: {
-                files: '<%= jshint.lib.src %>',
-                tasks: ['jshint:lib', 'nodeunit']
-            },
-            test: {
-                files: '<%= jshint.test.src %>',
-                tasks: ['jshint:test', 'nodeunit']
-            }
-        }
-    });
+	// Project configuration.
+	grunt.initConfig({
+		jshint: {
+			options: {
+				jshintrc: '.jshintrc'
+			},
+			gruntfile: {
+				src: 'Gruntfile.js'
+			},
+			lib: {
+				src: ['lib/*.js', 'lib/controllers/*.js', 'lib/routes/*.js']
+			},
+			test: {
+				src: ['test/**/*.js']
+			}
+		},
+		nodeunit: {
+			files: ['test/**/*.js']
+		},
+		watch: {
+			gruntfile: {
+				files: '<%= jshint.gruntfile.src %>',
+				tasks: ['jshint:gruntfile']
+			},
+			lib: {
+				files: '<%= jshint.lib.src %>',
+				tasks: ['jshint:lib', 'nodeunit']
+			},
+			test: {
+				files: '<%= jshint.test.src %>',
+				tasks: ['jshint:test', 'nodeunit']
+			}
+		}
+	});
 
-    // These plugins provide necessary tasks.
-    grunt.loadNpmTasks('grunt-contrib-nodeunit');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-nodeunit');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-    // Default task.
-    grunt.registerTask('default', ['jshint', 'nodeunit']);
+	// Default task.
+	grunt.registerTask('default', ['jshint', 'nodeunit']);
 
-    // Test task.
-    grunt.registerTask('test', ['jshint', 'nodeunit']);
+	// Test task.
+	grunt.registerTask('test', ['jshint', 'nodeunit']);
 };
