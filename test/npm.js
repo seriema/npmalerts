42e731b40c5657eec0548836402dd3b33383ffae
John-Philip Johansson <seriema@gmail.com>
2015-12-05 08:00:42 +0100

    Use npm-registry-client instead of npm internals
    Finally doing that todo added in cba8452 (Nov 23, 2014). :tada:

--- test/npm.js
+++ test/npm.js
@@ -1,35 +1,20 @@
 var npmMock = require('npm-registry-mock');
 var npmWrapper = require('../lib/npm.js');
 
-var loadOptions = { registry: "http://localhost:1331" };
+var mockUrl = 'http://localhost:1331/';
 var mockOptions = { port: 1331 };
 
 exports['npm'] = {
-  /*
-  'get latest package version': function(test) {
-    test.expect(2);
-    npmMock(mockOptions, function (s) {
-      npmWrapper.getLatestVersion('underscore', function(error, version) {
-        test.ok(!error);
-        test.ok(version);
-        test.done();
-        s.close();
-      }, loadOptions);
-    });
-  },*/
-
   'get all packages from npm': function(test) {
     test.expect(3);
-    npmMock(mockOptions, function (s) {
-      npmWrapper.load(loadOptions, function (error) {
+    npmMock(mockOptions, function (err, s) {
+      test.ok(!err);
+      npmWrapper.getLatestVersions(["underscore", "request"], function (error, versions) {
         test.ok(!error);
-        npmWrapper.getLatestVersions(["underscore", "request"], function (error, versions) {
-          test.ok(!error);
-          test.ok(versions);
-          test.done();
-          s.close();
-        });
-      });
+        test.ok(versions);
+        test.done();
+        s.close();
+      }, mockUrl);
     });
   }
 };
